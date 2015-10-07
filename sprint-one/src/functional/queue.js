beca6bf4fdbf7b9bede2883075387ff6001a9b3c
GregDungca <greg.dungca@gmail.com>
2015-10-07 12:25:48 -0700

    Completed functional > stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,26 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var length = 0;
 
   someInstance.enqueue = function(value) {
+    for ( var i = length - 1; i >= 0; i -- ) {
+      storage[i + 1] = storage[i];
+    }
+    storage[0] = value;
+    length ++;
   };
 
   someInstance.dequeue = function() {
+    var result = storage[length - 1];
+    if ( length > 0 ) {
+      length --;  
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
