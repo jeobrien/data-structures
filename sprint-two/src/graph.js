9adbb6b55da5183e7c089a2ed7b2ea9b227daaf0
jeobrien <j.e.obrien28@gmail.com>
2015-10-08 16:41:09 -0700

    Started on data structure for graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,21 +3,44 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.graph = [];
+  this.valueLookup = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.valueLookup[this.graph.length] = node;
+  this.graph.push([]);
+  this.graph[this.graph.length-1][this.graph.length-1] = 1;
+
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return _.contains(this.valueLookup, node);
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // The index of the graph that is associated with the valueLookup i.e.:
+  // // Loop through valueLookup, return key associated with nodeValue
+  var index;
+  for (var key in valueLookup) {
+    if (key === node) {
+      index = key;
+    } 
+  }
+  // index now equals the correct index in the graph
+  // make column and row special characters only
+  var nodeInGraph = graph[index];
+  for (var i = 0; i < nodeInGraph; i++) {
+    nodeInGraph[index][i] = "removed";
+    nodeInGraph[i][index] = "removed";
+  }
+
 };
 
 // ------------------------
@@ -44,4 +67,6 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+
+//[[], [], [], []]
 
