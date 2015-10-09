a551371ec70d80f04377cc9da4490cd49a85ca63
jeobrien <j.e.obrien28@gmail.com>
2015-10-08 20:50:36 -0700

    Passes all tests for Set in prototypal pattern.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (!_.contains(this._storageitem)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  if (_.contains(this._storage, item)) {
+    var index = this._storage.indexOf(item);
+  }
+  this._storage[index] = "removed";
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// add: Linear O(n) as contains is linear
+// contains is linear so O(n)
+// remove: as above.
+
+// Prototypal: Object.create with the object holding the shared methods
