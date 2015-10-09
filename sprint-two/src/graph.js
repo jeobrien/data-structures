00046fed4650c3ab60dda5e4d8ffb78f32964e0b
jeobrien <j.e.obrien28@gmail.com>
2015-10-08 20:09:21 -0700

    stuff

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -27,30 +27,44 @@ Graph.prototype.contains = function(node) {
 Graph.prototype.removeNode = function(node) {
   // The index of the graph that is associated with the valueLookup i.e.:
   // // Loop through valueLookup, return key associated with nodeValue
-  var index;
-  for (var key in valueLookup) {
-    if (key === node) {
-      index = key;
-    } 
-  }
+  
   // index now equals the correct index in the graph
   // make column and row special characters only
-  var nodeInGraph = graph[index];
-  for (var i = 0; i < nodeInGraph; i++) {
+  var index = this.findIndex(node);
+  var nodeInGraph = this.graph[index];
+  for (var i = 0; i < nodeInGraph.length; i++) {
     nodeInGraph[index][i] = "removed";
     nodeInGraph[i][index] = "removed";
   }
-
+  delete this.valueLookup[index];
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // This is a comment
+  
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  //set index of edge to equal 1
+  //  get index of fromNode
+  //    search for node with value of fromNode
+  //  get index of toNode
+  //    search for node with value of toNode
+
+};
+Graph.prototype.findIndex = function (value) {
+  var index;
+  for (var key in this.valueLookup) {
+    if (this.valueLookup[key] === value) {
+      index = key;
+      break;
+    }
+  }
+  return index;
 };
 
 // ------------------------
